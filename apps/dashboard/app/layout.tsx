7e375751a6f55148f94bbac3e8ee3da38f6ceb0d
Alvaro <alvgaona@gmail.com>
2025-07-10 23:16:21 -0300

    chores: apply format

--- apps/dashboard/app/layout.tsx
+++ apps/dashboard/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
-import { StackProvider, StackTheme } from "@stackframe/stack";
-import { stackServerApp } from "../stack";
+import { StackProvider, StackTheme } from '@stackframe/stack';
+import { stackServerApp } from '../stack';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 
@@ -26,7 +26,11 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}><StackProvider app={stackServerApp}><StackTheme>{children}</StackTheme></StackProvider></body>
+            <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+                <StackProvider app={stackServerApp}>
+                    <StackTheme>{children}</StackTheme>
+                </StackProvider>
+            </body>
         </html>
     );
 }
